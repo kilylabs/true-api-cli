35a162a00bf4ed0073b8ecf77ef7c52b00422bbe
Alexander Bogdanov <support@kily.ru>
2021-01-05 13:15:58 +0300

    feat(cises): allow to list received, owned or sent

--- src/Command/Markirovka/Cises/ListCommand.php
+++ src/Command/Markirovka/Cises/ListCommand.php
@@ -20,10 +20,14 @@ class ListCommand extends BaseCommand
 	public function options($opts)
 	{
         $opts->add('pg|product-group:','Product group')->isa('string');
-        $opts->add('l|limit:','Limit number of documents listed')->isa('number');
+        $opts->add('l|limit:','Limit number of codes listed')->isa('number');
         $opts->add('p|pretty','Pretty print');
         $opts->add('t|table','Print table');
         $opts->add('c|columns:','Comma-separated list of columns')->isa('string');
+
+        $opts->add('o|owned','List own cises');
+        $opts->add('r|received','List received cises');
+        $opts->add('s|sent','List sent cises');
 	}
 
 	public function execute($action=null)
@@ -33,12 +37,30 @@ class ListCommand extends BaseCommand
         //$resp = $this->parent->signedRequest('GET','https://ismp.crpt.ru/api/v3/facade/identifytools/listV2',[ - NOT WORKS
         //$resp = $this->parent->signedRequest('GET','cises/listV2',[ - NOT WORKS
         //$resp = $this->parent->signedRequest('POST','https://ismp.crpt.ru/api/v4/facade/cis/cis_list',[ - NOT WORKS
-        $resp = $this->parent->signedRequest('GET','https://ismp.crpt.ru/api/v3/facade/agent/received/list',[
-            'query'=>[
-                'pg'=>$opts->pg ?: 'lp',
-                'limit'=>$opts->limit ?: '10',
-            ],
-        ]);
+        if($opts->owned) {
+            $resp = $this->parent->signedRequest('GET','https://ismp.crpt.ru/api/v3/facade/identifytools/listV2',[
+                'query'=>[
+                    'pg'=>$opts->pg ?: 'lp',
+                    'limit'=>$opts->limit ?: '10',
+                ],
+            ]);
+        } elseif($opts->received) {
+            $resp = $this->parent->signedRequest('GET','https://ismp.crpt.ru/api/v3/facade/agent/received/list',[
+                'query'=>[
+                    'pg'=>$opts->pg ?: 'lp',
+                    'limit'=>$opts->limit ?: '10',
+                ],
+            ]);
+        } elseif($opts->sent) {
+            $resp = $this->parent->signedRequest('GET','https://ismp.crpt.ru/api/v3/facade/agent/given/list',[
+                'query'=>[
+                    'pg'=>$opts->pg ?: 'lp',
+                    'limit'=>$opts->limit ?: '10',
+                ],
+            ]);
+        } else {
+            throw new \Kily\API\TrueAPI\Cli\Exception\Exception("You need to supply -o, -r or -s options to list appropriate cises");
+        }
 
         if($opts->table) {
             $data = json_decode($resp->getBody()->__toString(),true);
